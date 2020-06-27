07f01a576a9d8c03e0956e7ef1d684c11c72d324
Alexandre Lemaitre <alexandre.lemaitre2@gmail.com>
2020-06-27 16:57:12 +0200

    start

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_100755) do
+ActiveRecord::Schema.define(version: 2020_06_27_143035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2020_06_25_100755) do
     t.bigint "trip_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["trip_id"], name: "index_steps_on_trip_id"
   end
 
