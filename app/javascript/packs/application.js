01cc9b394849749f7c079da0f06d13b6bb2e06b6
Linda Ait Habouche <lindaaithabouche@yahoo.fr>
2020-06-30 21:34:16 +0200

    formulaire

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,8 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+// app/assets/javascripts/application.js
+//= require jquery
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
