2a8a287d21605b0877feb9b6636eedd09c8e77e5
Adeline Ung <ungadeline93@hotmail.com>
2020-06-30 21:29:47 +0200

    affichage photos dans show

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,5 +1,8 @@
 class StepsController < ApplicationController
   def show
+    @trip = Trip.find(params[:trip_id])
+    @step = @trip.steps[params[:id].to_i - 1]
+    @blocks = @step.blocks
   end
 
   def new
