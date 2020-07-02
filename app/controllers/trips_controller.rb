b5686bf42ec9f5d9a2617ad02b0c1b11b7d0aaec
Bernadette d'Almeida <b.dalmeida@hotmail.fr>
2020-07-02 21:23:25 +0200

    modal trip show

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,6 @@
 class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
+    @photos = @trip.blocks.where(mediatype: "photos").map { |b| b.files }.flatten
   end
 end
