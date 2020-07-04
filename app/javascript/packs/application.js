f33524608d602261db78d2f71a2cf16be3175f5b
Linda Ait Habouche <lindaaithabouche@yahoo.fr>
2020-07-04 14:30:02 +0200

    form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,8 @@ require("channels")
 // External imports
 
 import "bootstrap";
+import {location} from "../plugins/location";
+location();
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -37,19 +39,24 @@ document.addEventListener('turbolinks:load', () => {
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-let d1 = document.getElementById("d1");
-let d2 = document.getElementById("d2");
-let p1 = document.getElementById("p1");
-let l1 = document.getElementById("l1");
 
-d1.addEventListener("mouseover", () => {p1.style.display = "block";});
-d1.addEventListener("mouseout", () => {p1.style.display = "none";});
 
-d2.onmouseover = aff;
-function aff(){
-  l1.style.display = "block";
-}
-d2.onmouseout = cac;
-function cac(){
-  l1.style.display = "none";
-}
+// a exporter et importer avec les variables
+// let d1 = document.getElementById("d1");
+// let d2 = document.getElementById("d2");
+// let p1 = document.getElementById("p1");
+// let l1 = document.getElementById("l1");
+// d1.addEventListener("mouseover", () => {p1.style.display = "block";});
+// d1.addEventListener("mouseout", () => {p1.style.display = "none";});
+
+
+// d2.onmouseover = aff;
+// function aff(){
+//   l1.style.display = "block";
+// }
+// d2.onmouseout = cac;
+// function cac(){
+//   l1.style.display = "none";
+// }
+
+
