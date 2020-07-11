104243523a165fc4be6d4b590b23c5c31d839a3a
Bernadette d'Almeida <b.dalmeida@hotmail.fr>
2020-07-11 17:57:40 +0200

    fav button done

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,9 +2,7 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
     @steps = @trip.steps.sort_by(&:id_in_its_trip)
-    # raise
     @photos = @trip.blocks.where(mediatype: "photos").map { |b| b.files }.flatten
     @contents = @trip.blocks.where(mediatype: "photos")
-    #photo.step.title
   end
 end
