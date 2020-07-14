8e2184c66225cee357606e0242da4f2e748e4170
Adeline Ung <ungadeline93@hotmail.com>
2020-07-14 11:42:46 +0200

    routes removed unused

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'favorite/index'
-  get 'favorite/create'
-  get 'favorite/destroy'
   devise_for :users
   root to: 'pages#home'
   get "profile", to: 'pages#profile'
   get "write", to: 'pages#write'
   get "explore", to: 'steps#explore'
-  get "favoris", to: 'favorite#index'
   patch "steps/:id", to: "steps#update"
 
   resources :trips, only: [:show, :new, :create] do
@@ -16,7 +12,6 @@ Rails.application.routes.draw do
   resources :steps, only: [:show] do
     resources :favorites, only: [:create]
   end
-
 
   resources :favorites, only: [:index, :destroy]
 end
