ed02d3a0c5dfcc7c0c7ca9da4763ab81fd07c74d
Bernadette d'Almeida <b.dalmeida@hotmail.fr>
2020-07-17 12:33:26 +0200

    tables Chatroom & Message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_222407) do
+ActiveRecord::Schema.define(version: 2020_07_17_101517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 2020_07_15_222407) do
     t.index ["step_id"], name: "index_blocks_on_step_id"
   end
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "step_id", null: false
@@ -60,6 +66,16 @@ ActiveRecord::Schema.define(version: 2020_07_15_222407) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_followers_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
@@ -112,6 +128,8 @@ ActiveRecord::Schema.define(version: 2020_07_15_222407) do
   add_foreign_key "favorites", "steps"
   add_foreign_key "favorites", "users"
   add_foreign_key "followers", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "steps", "trips"
   add_foreign_key "trips", "users"
 end
